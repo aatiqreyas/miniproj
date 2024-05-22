cf46d2637e287c8cb6f05b4f96a1b58b6d1549ee
aatiqreyas <contact@aatiq.in>
2024-05-22 15:25:44 +0530

    commit kinda works backend not tested yet

--- signin.php
+++ signin.php
@@ -1,82 +1,92 @@
 <?php
-//signup.php
+//signin.php
 include 'connect.php';
 include 'header.php';
-echo '<h3>Sign up</h3>';
-
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    // Display the signup form
-    echo '<form method="post" action="">
-    Username: <input type="text" name="user_name" required /> 
-    Password: <input type="password" name="user_pass" required> 
-    Password again: <input type="password" name="user_pass_check" required> 
-    E-mail: <input type="email" name="user_email" required> 
-    <input type="submit" value="Sign In" /> 
-    </form>';
-} else {
-    // Form has been posted, process the data
-    $errors = array();
-
-    // Validate username
-    if (isset($_POST['user_name'])) {
-        if (!ctype_alnum($_POST['user_name'])) {
-            $errors[] = 'The username can only contain letters and digits.';
-        }
-        if (strlen($_POST['user_name']) > 30) {
-            $errors[] = 'The username cannot be longer than 30 characters.';
-        }
-    } else {
-        $errors[] = 'The username field must not be empty.';
-    }
-
-    // Validate passwords
-    if (isset($_POST['user_pass'])) {
-        if ($_POST['user_pass'] != $_POST['user_pass_check']) {
-            $errors[] = 'The two passwords did not match.';
-        }
-    } else {
-        $errors[] = 'The password field cannot be empty.';
-    }
-
-    // Validate email
-    if (!isset($_POST['user_email']) || !filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'The email address is not valid.';
-    }
-
-    // Display errors if any
-    if (!empty($errors)) {
-        echo 'Uh-oh.. a couple of fields are not filled in correctly..';
-        echo '<ul>';
-        foreach ($errors as $key => $value) {
-            echo '<li>' . $value . '</li>';
-        }
-        echo '</ul>';
-    } else {
-        // Prepare the SQL statement with placeholders
-        $sql = "INSERT INTO users(user_name, user_pass, user_email, user_date, user_level) VALUES (?, ?, ?, NOW(), 0)";
-
-        // Create a prepared statement
-        $stmt = mysqli_prepare($conn, $sql);
-        if ($stmt) {
-            // Hash the password securely
-            $hashed_password = password_hash($_POST['user_pass'], PASSWORD_DEFAULT);
-
-            // Bind parameters to the prepared statement
-            mysqli_stmt_bind_param($stmt, 'sss', $_POST['user_name'], $hashed_password, $_POST['user_email']);
-
-            // Execute the prepared statement
-            $result = mysqli_stmt_execute($stmt);
-            if (!$result) {
-                echo 'Something went wrong while registering. Please try again later.';
-            } else {
-                echo 'Successfully registered. You can now <a href="signin.php">sign in</a> and start posting! :-)';
-            }
-            mysqli_stmt_close($stmt);
-        } else {
-            echo 'Could not prepare statement: ' . mysqli_error($conn);
-        }
-    }
+echo '<h3>Sign in</h3>';
+//first, check if the user is already signed in. If that is the case, there is no need to display this page
+if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true)
+{
+    echo 'You are already signed in, you can <a href="signout.php">sign out</a> if you want.';
 }
-
-include 'footer.php';
-?>
+else
+{
+    if($_SERVER['REQUEST_METHOD'] != 'POST')
+    {
+        /*the form hasn't been posted yet, display it
+note that the action="" will cause the form to post to the same page it is on */
+        echo '<form method="post" action=""> 
+Username: <input type="text" name="user_name" /> 
+Password: <input type="password" name="user_pass"> 
+<input type="submit" value="Sign in" /> 
+</form>';
+    }
+    else
+    {
+        /* so, the form has been posted, we'll process the data in three steps:
+1. Check the data
+2. Let the user refill the wrong fields (if necessary)
+3. Varify if the data is correct and return the correct response
+*/
+        $errors = array(); /* declare the array for later use */
+        if(!isset($_POST['user_name']))
+        {
+            $errors[] = 'The username field must not be empty.';
+        }
+        if(!isset($_POST['user_pass']))
+        {
+            $errors[] = 'The password field must not be empty.';
+        }
+        if(!empty($errors)) /*check for an empty array, if there are errors, they're in this array (note the ! operator)*/
+        {
+            echo 'Uh-oh.. a couple of fields are not filled in correctly..';
+            echo '<ul>';
+            foreach($errors as $key => $value) /* walk through the array so all the errors get displayed */
+            {
+                echo '<li>' . $value . '</li>'; /* this generates a nice error list */
+            }
+            echo '</ul>';
+        }
+        else
+        {
+            //the form has been posted without errors, so save it
+            //notice the use of mysql_real_escape_string, keep everything safe!
+            //also notice the sha1 function which hashes the password
+            $sql = "SELECT user_id, user_name, user_level FROM users WHERE user_name = ? AND user_pass = ?";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, 'ss', $_POST['user_name'], sha1($_POST['user_pass']));
+            $result = mysqli_stmt_execute($stmt);
+            if(!$result)
+            {
+                //something went wrong, display the error
+                echo 'Something went wrong while signing in. Please try again later.';
+                //echo mysql_error(); //debugging purposes, uncomment when needed
+            }
+            else
+            {
+                //the query was successfully executed, there are 2 possibilities
+                //1. the query returned data, the user can be signed in
+                //2. the query returned an empty result set, the credentials were wrong
+                $result = mysqli_stmt_get_result($stmt);
+                if (mysqli_num_rows($result) == 0) {
+                    {
+                        echo 'You have supplied a wrong user/password combination. Please try again.';
+                    }
+                else
+                    {
+                        //set the $_SESSION['signed_in'] variable to TRUE
+                        $_SESSION['signed_in'] = true;
+                        //we also put the user_id and user_name values in the $_SESSION, so we can use it at various pages
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            {
+                                $_SESSION['user_id'] 	= $row['user_id'];
+                                $_SESSION['user_name'] 	= $row['user_name'];
+                                $_SESSION['user_level'] = $row['user_level'];
+                            }
+                            echo 'Welcome, ' . $_SESSION['user_name'] . '. <a href="index.php">Proceed to the forum overview</a>.';
+                        }
+                    }
+                }
+            }
+        }
+        include 'footer.php';
+        ?>
